5110d5f5240ac6d23fc17753fc4577b00db30f7a
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-01-31 14:38:16 +0530

    Add files via upload

---
+++ Bank/src/main/java/bank/Bank.java
@@ -0,0 +1,34 @@
+package bank;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+class Account{
+	    private String accno;
+	    private String name;
+	    private long bal;
+	    Account(String an,String n,long b){
+	        this.accno=an;
+	        this.name=n;
+	        this.bal=b;
+	    }
+	    void deposite(long d){
+	        this.bal=this.bal+d;
+	    }
+	    void withdraw(long d){
+	        this.bal=this.bal-d;
+	    }
+	    long display(){
+	        return this.bal;
+	    }
+	}
+	public class Bank {
+	    public static void main(String[] args){
+	    	Logger l = Logger.getLogger("com.api.jar");
+	        Account b = new Account("2R44435" , "JANANI" ,200000);
+	        b.deposite(12000);
+	        b.withdraw(1000);
+	        long l1 = b.display();
+	        l.log(Level.INFO,()->"YOUR CURRENT BALANCE IS "+l1);
+	    }
+	    
+	}
+
